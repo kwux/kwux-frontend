b87de30ca10bf43f3b62377a1cf56475fb0ec1ab
Mo Omer <momer@apache.org>
2023-01-11 13:56:00 -0600

    Logo, nav

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# StaticController handles routes/actions for
+# static pages like the #index (home) page.
+class StaticController < ApplicationController
+  # Action for the index/home route.
+  def index; end
+end
