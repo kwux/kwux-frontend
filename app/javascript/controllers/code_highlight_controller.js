24258b24cfeb9ef532c977a51bbdcede4a620b53
Mo Omer <momer@apache.org>
2023-01-11 14:11:10 -0600

    Adding highlight.js and yarn deps

---
+++ app/javascript/controllers/code_highlight_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+import hljs from "highlight.js";
+
+export default class extends Controller {
+  connect() {
+    hljs.highlightAll();
+  }
+}
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +10,9 @@ module KwuxFrontend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # https://stackoverflow.com/questions/36602764/how-to-use-npm-packages-in-rails
+    config.assets.paths << Rails.root.join('node_modules')
 
     # Configuration for the application, engines, and railties goes here.
     #
