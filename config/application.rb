94e0c10ba9b115f0b1216b408452c48c286aaf4c
Mo Omer <mo@ipv7.com>
2023-01-17 07:03:15 -0600

    Bump version: 0.0.1-dev1 → 0.0.1-dev2

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module KwuxFrontend
   class Application < Rails::Application
     # Application Version
-    VERSION = "0.0.1-dev1"
+    VERSION = "0.0.1-dev2"
 
     # https://github.com/rails/tailwindcss-rails/issues/153
     config.assets.css_compressor = nil
