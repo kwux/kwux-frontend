4a6d12640f4b90763c46b0111fdf58afd96845e6
Mo Omer <mo@ipv7.com>
2023-01-17 15:32:02 -0600

    Update stimulus configuration

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-# https://stackoverflow.com/questions/70656496/how-to-install-highlight-js-to-rails-7-with-importmap
-pin "highlight.js", to: "https://ga.jspm.io/npm:highlight.js@11.7.0/es/index.js"
